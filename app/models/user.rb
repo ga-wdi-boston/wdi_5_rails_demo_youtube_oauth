e97ca1af11e481473b5fe1081e5ff7fa2a7f530e
David Fisher <tibbon@gmail.com>
2013-10-23 15:31:37 -0400

    AUTH WITH YOUTUBE WORKS

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2, :youtube]
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
@@ -16,4 +16,29 @@ class User < ActiveRecord::Base
     user
 	end
 
+  def self.find_for_youtube(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(email: data["email"]).first
+    if user
+      #Update the user's token
+      user.youtube_token = access_token["credentials"]["token"]
+      user.youtube_token_expires = access_token["credentials"]["expires_at"]
+      user.save
+    else
+      user = User.create(name: data["name"],
+           email: data["email"],
+           password: Devise.fraccess_tokeniendly_token[0,20],
+           youtube_token: access_token["credentials"]["token"],
+           youtube_token_expires: access_token["credentials"]["expires_at"]
+          )
+    end
+    user
+  end
+
 end
+
+
+# "credentials"=>
+#   {"token"=>"ya29.AHES6ZR0WruPZPvFSNgkYxG9j6iJYEduXy5Ep3nTX6nRiafHnGGesu44",
+#    "expires_at"=>1382559743,
+#    "expires"=>true},
